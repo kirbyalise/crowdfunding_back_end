3a4cae97559da01a1cd23298232d53b6bb9bc210
Thomson <Kirby.Thomson@riotinto.com>
2024-09-21 12:25:25 +1000

    Made changes to files

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -4,8 +4,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
-from .models import Project
-from .serializers import ProjectSerializer
+from .models import Project, Pledge
+from .serializers import ProjectSerializer, ProjectDetailSerializer
 
 class ProjectList(APIView):
     
@@ -15,18 +15,18 @@ class ProjectList(APIView):
         return Response(serializer.data)
     
     def post(self, request):
-       serializer = ProjectSerializer(data=request.data)
-       if serializer.is_valid():
-           serializer.save()
-           return Response(
-               serializer.data,
-               status=status.HTTP_201_CREATED
-           )
-       return Response(
-           serializer.errors,
-           status=status.HTTP_400_BAD_REQUEST
-       )
-       
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+        )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+    )
+
 class ProjectDetail(APIView):
     def get_object(self,pk):
         try:
@@ -37,8 +37,25 @@ class ProjectDetail(APIView):
         
     def get(self, request, pk):
         project = self.get_object(pk)
-        serializer = ProjectSerializer(project)
+        serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
-            
-        
-        
+
+class PledgeList(APIView):
+    def get(self, request):
+        pledges = Pledge.objects.all()
+        serializer = PledgeSerializer(pledges, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = PledgeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
