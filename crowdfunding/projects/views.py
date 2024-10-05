a39454e7aec50a27ae231f95a150529e0b838ffb
kirbythomson <kirby.thomson@riotinto.com>
2024-10-05 14:44:03 +1000

    Made changes and added permissions.py file

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from django.http import Http404
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
+from rest_framework import status, permissions
+from .permissions import IsOwnerOrReadOnly
 
 class ProjectList(APIView):
-    
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
     def get(self, request):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
@@ -28,9 +30,15 @@ class ProjectList(APIView):
     )
 
 class ProjectDetail(APIView):
+    
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
     def get_object(self,pk):
         try:
             project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request, project)
             return project
         except Project.DoesNotExist:
             raise Http404
@@ -39,6 +47,22 @@ class ProjectDetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+    
+
+    def put(self, request, pk):
+            project = self.get_object(pk)
+            serializer = ProjectDetailSerializer(
+                instance=project,
+                data=request.data,
+                partial=True
+            )
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
 class PledgeList(APIView):
     def get(self, request):
--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -4,6 +4,8 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
@@ -24,6 +26,7 @@ class CustomUserList(APIView):
             serializer.errors, 
             status=status.HTTP_400_BAD_REQUEST
         )
+
 class CustomUserDetail(APIView):
     def get_object(self, pk):
         try:
@@ -34,3 +37,19 @@ class CustomUserDetail(APIView):
         user = self.get_object(pk)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'email': user.email
+        })
