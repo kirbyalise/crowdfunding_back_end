033c3ccc2b02a4c57ee89f30cbf2709e99cfcce1
kirbythomson <kirby.thomson@riotinto.com>
2024-10-08 16:39:24 +1000

    Updates to code

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -47,7 +47,7 @@ class CustomAuthToken(ObtainAuthToken):
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
-        
+
         return Response({
             'token': token.key,
             'user_id': user.id,
