09b33fbf255aa1876c23ccd2f65203285f91c9ef
kirbythomson <kirby.thomson@riotinto.com>
2024-12-16 15:44:05 +1000

    Bug fixes

--- crowdfunding/projects/permissions.py
+++ crowdfunding/projects/permissions.py
@@ -4,9 +4,9 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return False
-
+        
         if request.user and request.user.is_superuser:
-            return False
+            return True
 
         return obj.owner == request.user
 
